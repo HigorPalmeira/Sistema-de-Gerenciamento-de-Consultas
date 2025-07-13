3f42e4a864f520ca86d2f650395fc09209dd1dde
Higor Palmeira <higorpresente05@gmail.com>
2025-07-12 21:11:03 -0300

    Iniciando Back-end

---
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/GerenciadorConsultasApiApplication.java
@@ -0,0 +1,13 @@
+package com.higorpalmeira.github.gerenciadorconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorConsultasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorConsultasApiApplication.class, args);
+	}
+
+}
