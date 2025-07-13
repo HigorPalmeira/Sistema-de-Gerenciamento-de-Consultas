bed013f9cc8c18453338be24f682c3b40d51ef3d
Higor Palmeira <higorpresente05@gmail.com>
2025-07-12 23:39:37 -0300

    Adicionando teste no SpecialityServiceTest
    Teste no método findSpecialityById().

--- gerenciador-consultas-api/src/test/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/SpecialityServiceTest.java
+++ gerenciador-consultas-api/src/test/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/SpecialityServiceTest.java
@@ -3,11 +3,13 @@ package com.higorpalmeira.github.gerenciadorconsultas.model.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
@@ -89,4 +91,53 @@ public class SpecialityServiceTest {
 		
 	}
 
+	@Nested
+	class getSpecialityById {
+		
+		@Test
+		@DisplayName("Should get speciality by id with success when optional is present")
+		void shouldGetSpecialityByIdWithSuccessWhenOptionalIsPresent() {
+			
+			// Arrange
+			var speciality = new Speciality(
+					UUID.randomUUID(),
+					"description",
+					Instant.now(),
+					null
+					);
+			
+			doReturn(Optional.of(speciality))
+				.when(specialityRepository)
+				.findById(uuidArgumentCaptor.capture());
+			
+			// Act
+			var output = specialityService.findSpecialityById(speciality.getId().toString());
+			
+			// Assert
+			assertTrue(output.isPresent());
+			assertEquals(speciality.getId(), uuidArgumentCaptor.getValue());
+			
+		}
+		
+		@Test
+		@DisplayName("Should get speciality by id with success when optional is empty")
+		void shouldGetSpecialityByIdWithSuccessWhenOptionalIsEmpty() {
+			
+			// Arrange
+			var specialityId = UUID.randomUUID();
+			doReturn(Optional.empty())
+				.when(specialityRepository)
+				.findById(uuidArgumentCaptor.capture());
+			
+			// Act
+			var output = specialityService.findSpecialityById(specialityId.toString());
+			
+			// Assert
+			assertTrue(output.isEmpty());
+			assertEquals(specialityId, uuidArgumentCaptor.getValue());
+			
+		}
+		
+	}
+	
 }
