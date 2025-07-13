8a944386bf448fcd50554c133a9a81c963972a84
Higor Palmeira <higorpresente05@gmail.com>
2025-07-13 14:59:03 -0300

    Update PatientService.java
    Criando método updatePatientById()

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreatePatientDto;
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdatePatientDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Patient;
 import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Gender.GenderType;
 import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Status.StatusType;
@@ -63,6 +64,53 @@ public class PatientService {
 	public List<Patient> listPatients() {
 		
 		return patientRepository.findAll();
+		
+	}
+	
+	public void updatePatientById(String patientId, UpdatePatientDto updatePatientDto) {
+		
+		var id = UUID.fromString(patientId);
+		var patientEntity = patientRepository.findById(id);
+		
+		if (patientEntity.isPresent()) {
+			
+			var patient = patientEntity.get();
+			
+			if (updatePatientDto.firstName() != null) {
+				patient.setFirstName(updatePatientDto.firstName());
+			}
+			
+			if (updatePatientDto.lastName() != null) {
+				patient.setLastName(updatePatientDto.lastName());
+			}
+			
+			if (updatePatientDto.cpf() != null && Validator.CPFValidation(updatePatientDto.cpf())) {
+				patient.setCpf(updatePatientDto.cpf());
+			}
+			
+			if (updatePatientDto.birthdate() != null) {
+				patient.setBirthdate(LocalDate.parse(updatePatientDto.birthdate(), DateTimeFormatter.ISO_LOCAL_DATE));
+			}
+			
+			if (updatePatientDto.gender() != null) {
+				patient.setGender(GenderType.fromType(updatePatientDto.gender()));
+			}
+			
+			if (updatePatientDto.status() != null) {
+				patient.setStatus(StatusType.fromType(updatePatientDto.status()));
+			}
+			
+			if (updatePatientDto.telephone() != null) {
+				patient.setTelephone(updatePatientDto.telephone());
+			}
+			
+			if (updatePatientDto.email() != null && Validator.EmailValidation(updatePatientDto.email())) {
+				patient.setEmail(updatePatientDto.email());
+			}
+			
+			patientRepository.save(patient);
+			
+		}
 		
 	}
 	
