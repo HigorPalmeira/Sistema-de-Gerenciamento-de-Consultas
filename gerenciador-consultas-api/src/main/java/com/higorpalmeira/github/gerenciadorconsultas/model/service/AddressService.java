18bc26bf0bd9bf10dea2069ae59e001aa57261aa
Higor Palmeira <higorpresente05@gmail.com>
2025-07-13 00:34:05 -0300

    Create AddressService.java

---
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/AddressService.java
@@ -0,0 +1,40 @@
+package com.higorpalmeira.github.gerenciadorconsultas.model.service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreateAddressDto;
+import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Address;
+import com.higorpalmeira.github.gerenciadorconsultas.model.repository.AddressRepository;
+
+@Service
+public class AddressService {
+	
+	private AddressRepository addressRepository;
+	
+	public AddressService(AddressRepository addressRepository) {
+		this.addressRepository = addressRepository;
+	}
+	
+	public UUID createAddress(CreateAddressDto createAddressDto) {
+		
+		var address = new Address(
+				createAddressDto.cep(),
+				createAddressDto.street(),
+				createAddressDto.complement(),
+				createAddressDto.neighborhood(),
+				createAddressDto.locality(),
+				createAddressDto.uf(),
+				Instant.now(),
+				null
+				);
+		
+		var addressSaved = addressRepository.save(address);
+		
+		return addressSaved.getId();
+		
+	}
+
+}
