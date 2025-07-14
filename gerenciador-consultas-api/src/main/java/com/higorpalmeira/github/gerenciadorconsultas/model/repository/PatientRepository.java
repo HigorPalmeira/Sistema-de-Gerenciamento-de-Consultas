6f13150e3345d76b79386f95a5720943f5168870
Higor Palmeira <higorpresente05@gmail.com>
2025-07-13 22:28:57 -0300

    Update PatientRepository.java
    Adicionando métodos de verificação de duplicidade de pacientes.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/PatientRepository.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/PatientRepository.java
@@ -9,5 +9,21 @@ import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Patient;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
+	
+	/*
+	 * Verifica se existe um paciente com o CPF fornecido.
+	 * 
+	 * @param cpf O CPF a ser verificado.
+	 * @return true se um paciente com este CPF existir, caso contrário false.
+	 * */
+	boolean existsByCpf(String cpf);
+	
+	/*
+	 * Verifica se existe um paciente com o E-mail fornecido.
+	 * 
+	 * @param email O E-mail a ser verifciado.
+	 * @return true se um paciente com este e-mail existir, caso contrário false.
+	 * */
+	boolean existsByEmail(String email);
 
 }
