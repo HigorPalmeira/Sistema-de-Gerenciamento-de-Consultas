24f0cfcf1a51d5c9341ebcb1647e53b010b97540
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 14:35:39 -0300

    Update Speciality.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/entity/Speciality.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/entity/Speciality.java
@@ -1,16 +1,21 @@
 package com.higorpalmeira.github.gerenciadorconsultas.model.entity;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,9 @@ public class Speciality {
 	
 	@Column (name = "description")
 	private String description;
+	
+	@OneToMany(mappedBy = "speciality", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+	private List<Doctor> doctors = new ArrayList<>();
 	
 	@CreationTimestamp
 	private Instant creationTimestamp;
@@ -79,6 +87,14 @@ public class Speciality {
 
 	public void setUpdateTimestamp(Instant updateTimestamp) {
 		this.updateTimestamp = updateTimestamp;
+	}
+
+	public List<Doctor> getDoctors() {
+		return doctors;
+	}
+
+	public void setDoctors(List<Doctor> doctors) {
+		this.doctors = doctors;
 	}
 
 }
