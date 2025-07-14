3d75034295fdc4777e33381694d24d70ec8004fb
Higor Palmeira <higorpresente05@gmail.com>
2025-07-13 22:24:11 -0300

    Update PatientService.java
    Iniciando a refatoração do Service.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreatePatientDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdatePatientDto;
@@ -15,161 +16,154 @@ import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Address;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Patient;
 import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Gender.GenderType;
 import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Status.StatusType;
+import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.InvalidDataException;
+import com.higorpalmeira.github.gerenciadorconsultas.model.mappers.PatientMapper;
 import com.higorpalmeira.github.gerenciadorconsultas.model.repository.PatientRepository;
 import com.higorpalmeira.github.gerenciadorconsultas.util.Validator;
 
 @Service
 public class PatientService {
-	
+
 	private PatientRepository patientRepository;
-	
-	public PatientService(PatientRepository patientRepository) {
+
+	private PatientMapper patientMapper;
+
+	public PatientService(PatientRepository patientRepository, PatientMapper patientMapper) {
 		this.patientRepository = patientRepository;
+		this.patientMapper = patientMapper;
 	}
-	
+
+	@Transactional(readOnly = false)
 	public UUID createPatient(CreatePatientDto createPatientDto) {
-		
-		if (Validator.CPFValidation(createPatientDto.cpf()) 
-				&& Validator.EmailValidation(createPatientDto.email())) {
-		
-			var addressEntity = createPatientDto.address();
-			var address = new Address(
-					addressEntity.cep(),
-					addressEntity.street(),
-					addressEntity.complement(),
-					addressEntity.neighborhood(),
-					addressEntity.locality(),
-					addressEntity.uf(),
-					Instant.now(),
-					null
-					);
-			
-			var patient = new Patient(
-					createPatientDto.firstName(),
-					createPatientDto.lastName(),
-					createPatientDto.cpf(),
-					LocalDate.parse(
-							createPatientDto.birthdate(), 
-							DateTimeFormatter.ISO_LOCAL_DATE),
-					GenderType.fromType(createPatientDto.gender()),
-					StatusType.ACTIVE,
-					createPatientDto.telephone(),
-					createPatientDto.email(),
-					address,
-					Instant.now(),
-					null
-					);
-			
-			var patientSaved = patientRepository.save(patient);
-			
-			return patientSaved.getPatientId();
+
+		if (!Validator.CPFValidation(createPatientDto.cpf())) {
+			throw new InvalidDataException("Invalid CPF.");
+		}
+
+		if (!Validator.EmailValidation(createPatientDto.email())) {
+			throw new InvalidDataException("Invalid e-mail format.");
 		}
 		
-		return null;
 		
+
+		var addressEntity = createPatientDto.address();
+		var address = new Address(addressEntity.cep(), addressEntity.street(), addressEntity.complement(),
+				addressEntity.neighborhood(), addressEntity.locality(), addressEntity.uf(), Instant.now(), null);
+
+		var patient = new Patient(createPatientDto.firstName(), createPatientDto.lastName(), createPatientDto.cpf(),
+				LocalDate.parse(createPatientDto.birthdate(), DateTimeFormatter.ISO_LOCAL_DATE),
+				GenderType.fromType(createPatientDto.gender()), StatusType.ACTIVE, createPatientDto.telephone(),
+				createPatientDto.email(), address, Instant.now(), null);
+
+		var patientSaved = patientRepository.save(patient);
+
+		return patientSaved.getPatientId();
+
+		// return null;
+
 	}
-	
+
 	public Optional<Patient> findPatientById(String patientId) {
-		
+
 		return patientRepository.findById(UUID.fromString(patientId));
-		
+
 	}
-	
+
 	public List<Patient> listPatients() {
-		
+
 		return patientRepository.findAll();
-		
+
 	}
-	
+
 	public void updatePatientById(String patientId, UpdatePatientDto updatePatientDto) {
-		
+
 		var id = UUID.fromString(patientId);
 		var patientEntity = patientRepository.findById(id);
-		
+
 		if (patientEntity.isPresent()) {
-			
+
 			var patient = patientEntity.get();
-			
+
 			if (updatePatientDto.firstName() != null) {
 				patient.setFirstName(updatePatientDto.firstName());
 			}
-			
+
 			if (updatePatientDto.lastName() != null) {
 				patient.setLastName(updatePatientDto.lastName());
 			}
-			
+
 			if (updatePatientDto.cpf() != null && Validator.CPFValidation(updatePatientDto.cpf())) {
 				patient.setCpf(updatePatientDto.cpf());
 			}
-			
+
 			if (updatePatientDto.birthdate() != null) {
 				patient.setBirthdate(LocalDate.parse(updatePatientDto.birthdate(), DateTimeFormatter.ISO_LOCAL_DATE));
 			}
-			
+
 			if (updatePatientDto.gender() != null) {
 				patient.setGender(GenderType.fromType(updatePatientDto.gender()));
 			}
-			
+
 			if (updatePatientDto.status() != null) {
 				patient.setStatus(StatusType.fromType(updatePatientDto.status()));
 			}
-			
+
 			if (updatePatientDto.telephone() != null) {
 				patient.setTelephone(updatePatientDto.telephone());
 			}
-			
+
 			if (updatePatientDto.email() != null && Validator.EmailValidation(updatePatientDto.email())) {
 				patient.setEmail(updatePatientDto.email());
 			}
-			
+
 			if (updatePatientDto.address() != null) {
-				
+
 				var addressEntity = updatePatientDto.address();
 				var address = patient.getAddress();
-				
+
 				if (addressEntity.cep() != null) {
 					address.setCep(addressEntity.cep());
 				}
-				
+
 				if (addressEntity.street() != null) {
 					address.setStreet(addressEntity.street());
 				}
-				
+
 				if (addressEntity.complement() != null) {
 					address.setComplement(addressEntity.complement());
 				}
-				
+
 				if (addressEntity.neighborhood() != null) {
 					address.setNeighborhood(addressEntity.neighborhood());
 				}
-				
+
 				if (addressEntity.locality() != null) {
 					address.setLocality(addressEntity.locality());
 				}
-				
+
 				if (addressEntity.uf() != null) {
 					address.setUf(addressEntity.uf());
 				}
-				
+
 				patient.setAddress(address);
-				
+
 			}
-			
+
 			patientRepository.save(patient);
-			
+
 		}
-		
+
 	}
-	
+
 	public void deletePatientById(String patientId) {
-		
+
 		var id = UUID.fromString(patientId);
 		var patientExists = patientRepository.existsById(id);
-		
+
 		if (patientExists) {
 			patientRepository.deleteById(id);
 		}
-		 
+
 	}
 
 }
