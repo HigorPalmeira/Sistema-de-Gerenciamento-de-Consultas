07a86b57b427ec5aaf0999fa778cd429406212a7
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 14:02:54 -0300

    Update AddressService.java
    Refatorando método de atualização.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/AddressService.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/AddressService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreateAddressDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdateAddressDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Address;
+import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.ResourceNotFoundException;
 import com.higorpalmeira.github.gerenciadorconsultas.model.mappers.AddressMapper;
 import com.higorpalmeira.github.gerenciadorconsultas.model.repository.AddressRepository;
 
@@ -54,39 +55,11 @@ public class AddressService {
 	public void updateAddressById(String addressId, UpdateAddressDto updateAddressDto) {
 		
 		var id = UUID.fromString(addressId);
-		var addressEntity = addressRepository.findById(id);
+		var addressEntity = addressRepository
+				.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Address not found with ID: " + id));
 		
-		if (addressEntity.isPresent()) {
-			
-			var address = addressEntity.get();
-			
-			if (updateAddressDto.cep() != null) {
-				address.setCep(updateAddressDto.cep());
-			}
-			
-			if (updateAddressDto.street() != null) {
-				address.setStreet(updateAddressDto.street());
-			}
-			
-			if (updateAddressDto.complement() != null) {
-				address.setComplement(updateAddressDto.complement());
-			}
-			
-			if (updateAddressDto.neighborhood() != null) {
-				address.setNeighborhood(updateAddressDto.neighborhood());
-			}
-			
-			if (updateAddressDto.locality() != null) {
-				address.setLocality(updateAddressDto.locality());
-			}
-			
-			if (updateAddressDto.uf() != null) {
-				address.setUf(updateAddressDto.uf());
-			}
-			
-			addressRepository.save(address);
-			
-		}
+		addressMapper.updateEntityFromDto(addressEntity, updateAddressDto);
 		
 	}
 	
