ec9eade6867cd6abf5e51a7fa7498ccf94231316
Higor Palmeira <higorpresente05@gmail.com>
2025-07-13 23:18:54 -0300

    Update PatientService.java
    Refatorando os métodos.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/PatientService.java
@@ -1,7 +1,5 @@
 package com.higorpalmeira.github.gerenciadorconsultas.model.service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,10 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreatePatientDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdatePatientDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Patient;
-import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Gender.GenderType;
-import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Status.StatusType;
 import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.DataConflictException;
 import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.InvalidDataException;
+import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.ResourceNotFoundException;
 import com.higorpalmeira.github.gerenciadorconsultas.model.mappers.PatientMapper;
 import com.higorpalmeira.github.gerenciadorconsultas.model.repository.PatientRepository;
 import com.higorpalmeira.github.gerenciadorconsultas.util.Validator;
@@ -32,7 +29,7 @@ public class PatientService {
 		this.patientMapper = patientMapper;
 	}
 
-	@Transactional(readOnly = false)
+	@Transactional
 	public UUID createPatient(CreatePatientDto createPatientDto) {
 
 		if (!Validator.CPFValidation(createPatientDto.cpf())) {
@@ -73,87 +70,39 @@ public class PatientService {
 
 	}
 
-	@Transactional(readOnly = false)
+	@Transactional
 	public void updatePatientById(String patientId, UpdatePatientDto updatePatientDto) {
 
 		var id = UUID.fromString(patientId);
-		var patientEntity = patientRepository.findById(id);
-
-		if (patientEntity.isPresent()) {
-
-			var patient = patientEntity.get();
-
-			if (updatePatientDto.firstName() != null) {
-				patient.setFirstName(updatePatientDto.firstName());
-			}
-
-			if (updatePatientDto.lastName() != null) {
-				patient.setLastName(updatePatientDto.lastName());
-			}
-
-			if (updatePatientDto.cpf() != null && Validator.CPFValidation(updatePatientDto.cpf())) {
-				patient.setCpf(updatePatientDto.cpf());
-			}
-
-			if (updatePatientDto.birthdate() != null) {
-				patient.setBirthdate(LocalDate.parse(updatePatientDto.birthdate(), DateTimeFormatter.ISO_LOCAL_DATE));
-			}
-
-			if (updatePatientDto.gender() != null) {
-				patient.setGender(GenderType.fromType(updatePatientDto.gender()));
-			}
-
-			if (updatePatientDto.status() != null) {
-				patient.setStatus(StatusType.fromType(updatePatientDto.status()));
-			}
-
-			if (updatePatientDto.telephone() != null) {
-				patient.setTelephone(updatePatientDto.telephone());
-			}
-
-			if (updatePatientDto.email() != null && Validator.EmailValidation(updatePatientDto.email())) {
-				patient.setEmail(updatePatientDto.email());
-			}
-
-			if (updatePatientDto.address() != null) {
-
-				var addressEntity = updatePatientDto.address();
-				var address = patient.getAddress();
-
-				if (addressEntity.cep() != null) {
-					address.setCep(addressEntity.cep());
+		var patientEntity = patientRepository
+				.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Patient not found with ID: " + id));
+		
+		if (updatePatientDto.cpf() != null) {
+			patientRepository.findByCpf(updatePatientDto.cpf()).ifPresent(existingPatient -> {
+				if (!existingPatient.getPatientId().equals(patientEntity.getPatientId())) {
+					throw new DataConflictException("CPF is already in use by another patient.");
 				}
-
-				if (addressEntity.street() != null) {
-					address.setStreet(addressEntity.street());
-				}
-
-				if (addressEntity.complement() != null) {
-					address.setComplement(addressEntity.complement());
-				}
-
-				if (addressEntity.neighborhood() != null) {
-					address.setNeighborhood(addressEntity.neighborhood());
-				}
-
-				if (addressEntity.locality() != null) {
-					address.setLocality(addressEntity.locality());
-				}
-
-				if (addressEntity.uf() != null) {
-					address.setUf(addressEntity.uf());
-				}
-
-				patient.setAddress(address);
-
-			}
-
-			patientRepository.save(patient);
-
+			});
+			
+			patientEntity.setCpf(updatePatientDto.cpf());
 		}
+		
+		if (updatePatientDto.email() != null) {
+			patientRepository.findByEmail(updatePatientDto.email()).ifPresent(existingPatient -> {
+				if (!existingPatient.getPatientId().equals(patientEntity.getPatientId())) {
+					throw new DataConflictException("Email is already in use by another patient.");
+				}
+			});
+			
+			patientEntity.setEmail(updatePatientDto.email());
+		}
+		
+		patientMapper.updateEntityFromDto(patientEntity, updatePatientDto);
 
 	}
 
+	@Transactional
 	public void deletePatientById(String patientId) {
 
 		var id = UUID.fromString(patientId);
