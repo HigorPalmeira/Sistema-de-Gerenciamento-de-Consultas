c8fc990ba646da10d896675e4acf706847cc5a22
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 13:52:09 -0300

    Update AddressService.java
    Refatorando os métodos

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/AddressService.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/service/AddressService.java
@@ -1,15 +1,16 @@
 package com.higorpalmeira.github.gerenciadorconsultas.model.service;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreateAddressDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdateAddressDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Address;
+import com.higorpalmeira.github.gerenciadorconsultas.model.mappers.AddressMapper;
 import com.higorpalmeira.github.gerenciadorconsultas.model.repository.AddressRepository;
 
 @Service
@@ -17,22 +18,17 @@ public class AddressService {
 	
 	private AddressRepository addressRepository;
 	
-	public AddressService(AddressRepository addressRepository) {
+	private AddressMapper addressMapper;
+	
+	public AddressService(AddressRepository addressRepository, AddressMapper addressMapper) {
 		this.addressRepository = addressRepository;
+		this.addressMapper = addressMapper;
 	}
 	
+	@Transactional
 	public UUID createAddress(CreateAddressDto createAddressDto) {
 		
-		var address = new Address(
-				createAddressDto.cep(),
-				createAddressDto.street(),
-				createAddressDto.complement(),
-				createAddressDto.neighborhood(),
-				createAddressDto.locality(),
-				createAddressDto.uf(),
-				Instant.now(),
-				null
-				);
+		var address = addressMapper.toEntity(createAddressDto);
 		
 		var addressSaved = addressRepository.save(address);
 		
@@ -40,18 +36,21 @@ public class AddressService {
 		
 	}
 	
+	@Transactional
 	public Optional<Address> findAddressById(String addressId) {
 		
 		return addressRepository.findById(UUID.fromString(addressId));
 		
 	}
 	
+	@Transactional
 	public List<Address> listAddresses() {
 		
 		return addressRepository.findAll();
 		
 	}
 	
+	@Transactional
 	public void updateAddressById(String addressId, UpdateAddressDto updateAddressDto) {
 		
 		var id = UUID.fromString(addressId);
@@ -91,6 +90,7 @@ public class AddressService {
 		
 	}
 	
+	@Transactional
 	public void deleteAddressById(String addressId) {
 		
 		var id = UUID.fromString(addressId);
