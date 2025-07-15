35f9c0d4cf515373ebdc566a1762b76985c6fb94
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 23:52:35 -0300

    Update Consultation.java
    Adicionando relação paciente/médico e consultas.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/entity/Consultation.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/entity/Consultation.java
@@ -7,11 +7,16 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Status.StatusConsultationType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,11 +30,22 @@ public class Consultation {
 	@Column(name = "date_time")
 	private LocalDateTime dateTime;
 	
+	@Column(name = "status")
+	private StatusConsultationType status;
+	
 	@Column(name = "observations")
 	private String observations;
 	
 	@Column(name = "value")
 	private float value;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "doctor_id")
+	private Doctor doctor;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "patient_id")
+	private Patient patient;
 	
 	@CreationTimestamp
 	private Instant creationTimestamp;
