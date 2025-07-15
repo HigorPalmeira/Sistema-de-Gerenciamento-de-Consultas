7905957feff3a421d50560329464aea11f85f642
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 21:14:12 -0300

    Create DoctorRepository.java

---
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/DoctorRepository.java
@@ -0,0 +1,46 @@
+package com.higorpalmeira.github.gerenciadorconsultas.model.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Doctor;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
+	
+	/*
+	 * Verifica se existe um médico com o CRM fornecido.
+	 * 
+	 * @param crm O CRM a ser verificado.
+	 * @return true se um médico com este CPF existir, caso contrário false.
+	 * */
+	boolean existsByCrm(String crm);
+	
+	/*
+	 * Verifica se existe um médico com o E-mail fornecido.
+	 * 
+	 * @param email O E-mail a ser verificado.
+	 * @return true se um médico existir com este e-mail, caso contrário false.
+	 * */
+	boolean existsByEmail(String email);
+	
+	/*
+	 * Busca por um médico com o CRM fornecido.
+	 * 
+	 * @param crm O CRM a ser procurado.
+	 * @return Optional com Doctor presente se o médico for encontrado, caso contrário um Optional vazio.
+	 * */
+	Optional<Doctor> findByCrm(String crm);
+	
+	/*
+	 * Busca por um médico com o E-mail fornecido.
+	 * 
+	 * @param email O E-mail a ser procurado.
+	 * @return Optional com Doctor presente se o médico for encontrado, caso contrário um Optional vazio.
+	 * */
+	Optional<Doctor> findByEmail(String email);
+
+}
