507d00e68c5c73b76539deb3d46173a7db11b79d
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 21:23:28 -0300

    Update SpecialityRepository.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/SpecialityRepository.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/SpecialityRepository.java
@@ -9,5 +9,13 @@ import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Speciality;
 
 @Repository
 public interface SpecialityRepository extends JpaRepository<Speciality, UUID> {
+	
+	/*
+	 * Verifica se existe uma especialidade com a descrição fornecida.
+	 * 
+	 * @param description A descrição da especialidade a ser verificado.
+	 * @return true se uma especialidade com esta descrição existir, caso contrário false.
+	 * */
+	boolean existsByDescription(String description);
 
 }
