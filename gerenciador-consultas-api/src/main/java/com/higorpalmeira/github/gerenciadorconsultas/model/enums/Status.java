57f9de00453f8d025c2a9f95fcded55c3a63921b
Higor Palmeira <higorpresente05@gmail.com>
2025-07-14 23:47:26 -0300

    Update Status.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/enums/Status.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/enums/Status.java
@@ -1,5 +1,7 @@
 package com.higorpalmeira.github.gerenciadorconsultas.model.enums;
 
+import com.higorpalmeira.github.gerenciadorconsultas.model.exceptions.InvalidDataException;
+
 public class Status {
 	
 	public enum StatusAccountType {
@@ -19,7 +21,31 @@ public class Status {
 				}
 			}
 			
-			throw new UnsupportedOperationException("The type is not supported!");
+			throw new InvalidDataException("The type is not supported!");
+		}
+	}
+	
+	public enum StatusConsultationType {
+		SCHEDULED("SCHEDULED"),
+		HELD("HELD"),
+		RESCHEDULED("RESCHEDULED"),
+		CANCELED("CANCELED"),
+		MISSING("MISSING");
+		
+		private String type;
+		
+		StatusConsultationType(String aType) {
+			this.type = aType;
+		}
+		
+		public static StatusConsultationType fromType(String aType) {
+			for (StatusConsultationType status : StatusConsultationType.values()) {
+				if (status.type.equalsIgnoreCase(aType)) {
+					return status;
+				}
+			}
+			
+			throw new InvalidDataException("The type is not supported!");
 		}
 	}
 
