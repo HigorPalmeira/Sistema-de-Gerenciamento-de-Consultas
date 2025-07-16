399b7e883aae34b64f58c1827e2ebac5a49db56b
Higor Palmeira <higorpresente05@gmail.com>
2025-07-16 11:55:07 -0300

    Update ConsultationController.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/controller/ConsultationController.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/controller/ConsultationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.CreateConsultationDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.OutputSimpleConsultationDto;
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.UpdateConsultationDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Consultation;
 import com.higorpalmeira.github.gerenciadorconsultas.model.service.ConsultationService;
 
