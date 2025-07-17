1501515ae979e4b87c5d9ff0c1e9f8367e8ec045
Higor Palmeira <higorpresente05@gmail.com>
2025-07-17 00:56:17 -0300

    Update SpecialityMapper.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/mappers/SpecialityMapper.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/mappers/SpecialityMapper.java
@@ -14,15 +14,33 @@ import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Speciality;
 @Mapper(componentModel = "spring")
 public interface SpecialityMapper {
 	
+	/*
+	 * Cria uma entidade 'Speciality' com os dados do DTO.
+	 * 
+	 * @param createSpecialityDto Objeto com os dados para criação da entidade.
+	 * @return Speciality Entidade criada a partir dos dados do DTO.
+	 * */
 	@Mapping(target = "id", ignore = true)
 	@Mapping(target = "creationTimestamp", ignore = true)
 	@Mapping(target = "updateTimestamp", ignore = true)
 	@Mapping(target = "doctors", ignore = true)
 	Speciality createToSpeciality(CreateSpecialityDto createSpecialityDto);
 	
+	/*
+	 * Cria um DTO de saída simples a partir da entidade 'Speciality'.
+	 * 
+	 * @param speciality Entidade a ser transformada.
+	 * @return SimpleOutputSpecialityDto DTO de saída simples criado.
+	 * */
 	@Mapping(target = "associateDoctors", expression = "java(speciality.getDoctors() != null ? speciality.getDoctors().size() : 0)")
 	SimpleOutputSpecialityDto specialityToSimpleOutputSpecialityDto(Speciality speciality);
 	
+	/**
+     * Atualiza a entidade 'Speciality' com os dados não nulos do DTO.
+     * 
+     * @param updateSpecialityDto O objeto com os dados para atualização.
+     * @param speciality A entidade que será atualizada (carregada do banco).
+     */
 	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 	@Mapping(target = "id", ignore = true)
 	@Mapping(target = "creationTimestamp", ignore = true)
