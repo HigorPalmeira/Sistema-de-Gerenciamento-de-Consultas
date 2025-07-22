65b2fa5889ca49ce9844559b6e6e5f7950825677
Higor Palmeira <higorpresente05@gmail.com>
2025-07-22 16:36:21 -0300

    Update MedicoService.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/service/MedicoService.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/service/MedicoService.java
@@ -11,7 +11,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 /**
