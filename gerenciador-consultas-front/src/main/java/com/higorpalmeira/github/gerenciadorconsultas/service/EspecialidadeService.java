c4c02d248ccf4e3ea4278b44b847d30a7a729f77
Higor Palmeira <higorpresente05@gmail.com>
2025-07-22 16:47:08 -0300

    Create EspecialidadeService.java

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/service/EspecialidadeService.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.higorpalmeira.github.gerenciadorconsultas.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.SaidaSimplesEspecialidadeDto;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
+
+/**
+ *
+ * @author higor
+ */
+public class EspecialidadeService {
+    
+    private final String URL_API = "http://localhost:8080/v1/especialidade";
+    
+    public SaidaSimplesEspecialidadeDto getSaidaSimplesEspecialidadeDto(UUID id) {
+        
+        SaidaSimplesEspecialidadeDto especialidadeDto = new SaidaSimplesEspecialidadeDto();
+        
+        try {
+            
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(URL_API + "/" + id.toString()))
+                    .GET()
+                    .build();
+            
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            
+            ObjectMapper mapper = new ObjectMapper();
+            
+            especialidadeDto = mapper.readValue(response.body(), SaidaSimplesEspecialidadeDto.class);
+            
+        } catch(IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return especialidadeDto;
+        
+    }
+    
+}
