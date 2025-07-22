990790eb0dcbe09dac22710a9c60b9061a12807a
Higor Palmeira <higorpresente05@gmail.com>
2025-07-21 23:37:15 -0300

    Iniciando requisição da API

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/external/EspecialidadeDto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.higorpalmeira.github.gerenciadorconsultas.model.dto.external;
+
+/**
+ *
+ * @author higor
+ */
+
+public class EspecialidadeDto {
+    
+}
