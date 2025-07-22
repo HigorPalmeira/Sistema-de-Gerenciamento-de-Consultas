cfd5931838a36a0b7d551d33ded58e5c1c904cc1
Higor Palmeira <higorpresente05@gmail.com>
2025-07-21 22:25:47 -0300

    Update MedicoMapper.java

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/mappers/MedicoMapper.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/mappers/MedicoMapper.java
@@ -7,12 +7,13 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.create.CriarMedicoDto;
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.output.SaidaDetalhadaMedicoDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.output.SaidaSimplesMedicoDto;
 import com.higorpalmeira.github.gerenciadorconsultas.model.dto.update.AtualizarMedicoDto;
-import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Medico;
 import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Especialidade;
+import com.higorpalmeira.github.gerenciadorconsultas.model.entity.Medico;
 
-@Mapper(componentModel = "spring", uses = {EspecialidadeMapper.class})
+@Mapper(componentModel = "spring", uses = {EspecialidadeMapper.class, ConsultaMapper.class})
 public interface MedicoMapper {
 	
 	@Mapping(target = "id", ignore = true)
@@ -24,6 +25,8 @@ public interface MedicoMapper {
 
 	@Mapping(target = "consultas", expression = "java(doctor.getConsultas() != null ? doctor.getConsultas().size() : 0)")
 	SaidaSimplesMedicoDto medicoParaSaidaSimplesMedicoDto(Medico medico);
+	
+	SaidaDetalhadaMedicoDto medicoParaSaidaDetalhadaMedicoDto(Medico medico);
 	
 	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 	@Mapping(target = "id", ignore = true)
