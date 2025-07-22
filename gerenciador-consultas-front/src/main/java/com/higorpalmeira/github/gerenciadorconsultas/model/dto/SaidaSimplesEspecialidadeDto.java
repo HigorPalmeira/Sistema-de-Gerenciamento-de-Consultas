e348ca4c5bfa8d96bba691402d34e0dedbb629d1
Higor Palmeira <higorpresente05@gmail.com>
2025-07-22 16:37:42 -0300

    Create SaidaSimplesEspecialidadeDto.java

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/SaidaSimplesEspecialidadeDto.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author higor
+ */
+public class SaidaSimplesEspecialidadeDto {
+
+    private UUID id;
+
+    private String descricao;
+
+    private int medicosAssociados;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getMedicosAssociados() {
+        return medicosAssociados;
+    }
+
+    public void setMedicosAssociados(int medicosAssociados) {
+        this.medicosAssociados = medicosAssociados;
+    }
+
+    
+}
