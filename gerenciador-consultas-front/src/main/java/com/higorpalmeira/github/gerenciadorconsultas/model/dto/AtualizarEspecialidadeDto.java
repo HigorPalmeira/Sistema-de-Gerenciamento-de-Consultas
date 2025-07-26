a8ff69e4aa2695f15ab8a705d0f5f737a9edf0f5
Higor Palmeira <higorpresente05@gmail.com>
2025-07-26 13:11:29 -0300

    Create AtualizarEspecialidadeDto.java

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarEspecialidadeDto.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
+
+/**
+ *
+ * @author higor
+ */
+public class AtualizarEspecialidadeDto {
+    
+    private String descricao;
+
+    public AtualizarEspecialidadeDto() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
