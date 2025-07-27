ca20d39c13258c0789ccbdf73d7a99f73750aa17
Higor Palmeira <higorpresente05@gmail.com>
2025-07-26 22:49:37 -0300

    Update MedicoClient.java
    Criando método de listagem de médicos saída simples.

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/client/MedicoClient.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/client/MedicoClient.java
@@ -72,4 +72,17 @@ public class MedicoClient {
         
     }
     
+    public HttpResponse<String> listarSaidaSimplesMedicoDto() throws IOException, InterruptedException {
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL_API))
+                .GET()
+                .build();
+        
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        return response;
+        
+    }
+    
 }
