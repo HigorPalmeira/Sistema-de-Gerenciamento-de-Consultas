92b75e4d88ad2583ae04e1086fe90b45f193883b
Higor Palmeira <higorpresente05@gmail.com>
2025-07-26 22:38:20 -0300

    Update AtualizarMedicoDto.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarMedicoDto.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarMedicoDto.java
@@ -4,6 +4,7 @@
  */
 package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
 
+import com.higorpalmeira.github.gerenciadorconsultas.model.enums.TipoStatus.TipoStatusConta;
 import java.util.UUID;
 
 /**
@@ -26,4 +27,64 @@ public class AtualizarMedicoDto {
 
     private UUID especialidadeId;
 
+    public AtualizarMedicoDto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoStatusConta getStatus() {
+        return status;
+    }
+
+    public void setStatus(TipoStatusConta status) {
+        this.status = status;
+    }
+
+    public UUID getEspecialidadeId() {
+        return especialidadeId;
+    }
+
+    public void setEspecialidadeId(UUID especialidadeId) {
+        this.especialidadeId = especialidadeId;
+    }
+
+    
 }
