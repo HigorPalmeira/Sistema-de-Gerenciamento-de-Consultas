835c5eabcd307dac6f50dbf8d8a31ba94facac3a
Higor Palmeira <higorpresente05@gmail.com>
2025-07-27 20:46:55 -0300

    Create CriarEnderecoDto.java

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/CriarEnderecoDto.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
+
+/**
+ *
+ * @author higor
+ */
+public class CriarEnderecoDto {
+
+    private String cep;
+
+    private String rua;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String localidade;
+
+    private String uf;
+
+    public CriarEnderecoDto() {
+    }
+
+    public CriarEnderecoDto(String cep, String rua, String complemento, String bairro, String localidade, String uf) {
+        this.cep = cep;
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+}
