fd9eaeb55075b7335581d51db9651a6d0ec293c7
Higor Palmeira <higorpresente05@gmail.com>
2025-07-27 21:50:59 -0300

    Update PacienteService.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/service/PacienteService.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/service/PacienteService.java
@@ -13,8 +13,6 @@ import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
