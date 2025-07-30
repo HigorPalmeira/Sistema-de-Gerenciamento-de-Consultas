10099b14ffc60f5af836401b08aa81179649d656
Higor Palmeira <higorpresente05@gmail.com>
2025-07-30 14:58:06 -0300

    Update Validador.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/util/Validador.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/util/Validador.java
@@ -28,10 +28,10 @@ public class Validador {
             = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@"
             + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     private static final Pattern PATTERN_EMAIL = Pattern.compile(EMAIL_REGEX);
-    
+
     private static final String TELEFONE_REGEX = "^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[0-9])[0-9]{3}\\-?[0-9]{4}$";
     private static final Pattern PATTERN_TELEFONE = Pattern.compile(TELEFONE_REGEX);
-    
+
     private static final String CEP_REGEX = "^(\\d{2}\\.\\d{3}-\\d{3}|\\d{5}-\\d{3}|\\d{8})$";
     private static final Pattern PATTERN_CEP = Pattern.compile(CEP_REGEX);
 
@@ -75,28 +75,75 @@ public class Validador {
         return matcher.matches();
 
     }
-    
+
     public static boolean isTelefone(final String input) {
-        
+
         if (input == null) {
             return false;
         }
-        
+
         Matcher matcher = PATTERN_TELEFONE.matcher(input);
-        
+
         return matcher.matches();
-        
+
     }
 
     public static boolean isCep(final String input) {
-        
+
         if (input == null) {
             return false;
         }
-        
+
         Matcher matcher = PATTERN_CEP.matcher(input);
-        
+
         return matcher.matches();
-        
+
     }
+
+    public static boolean isCpf(final String input) {
+
+        if (input == null || input.isBlank()) {
+            return false;
+        }
+
+        String cpf = input.trim().replace(".", "").replace("-", "");
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        char[] arrCpf = cpf.toCharArray();
+        int firstDigit = 0;
+        int j = 10;
+
+        for (int i = 0; i < 9; i++) {
+            firstDigit += Character.getNumericValue(arrCpf[i]) * j--;
+        }
+        firstDigit = (firstDigit * 10) % 11;
+
+        if (firstDigit != Character.getNumericValue(arrCpf[9])) {
+            return false;
+        }
+
+        int secondDigit = 0;
+        j = 11;
+
+        for (int i = 0; i < 10; i++) {
+            secondDigit += Character.getNumericValue(arrCpf[i]) * j--;
+        }
+        secondDigit = (secondDigit * 10) % 11;
+
+        if (secondDigit != Character.getNumericValue(arrCpf[10])) {
+            return false;
+        }
+
+        char initialChar = arrCpf[0];
+        for (int i = 1; i < arrCpf.length; i++) {
+            if (initialChar != arrCpf[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
