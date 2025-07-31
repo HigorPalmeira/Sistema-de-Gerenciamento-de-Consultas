dc0a468c80a187660f8c76e321d958ac227b78a2
Higor Palmeira <higorpresente05@gmail.com>
2025-07-31 10:46:05 -0300

    Update Formatter.java
    Adicionando método de formatação do CPF

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/util/Formatter.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/util/Formatter.java
@@ -11,5 +11,20 @@ public class Formatter {
 		return input.replaceAll("[ .()-]", "");
 		
 	}
+	
+	public static String ofCpf(String cpf) {
+		
+		if (cpf == null || cpf.isBlank()) return cpf;
+		
+		String cpfDigitos = cpf.replaceAll("[^\\d]", "");
+		
+		if (cpfDigitos.length() != 11) return cpf;
+		
+		String regex = "^(\\d{3})(\\d{3})(\\d{3})(\\d{2})$";
+		String replacement = "$1.$2.$3-$4";
+		
+		return cpfDigitos.replaceAll(regex, replacement);
+		
+	}
 
 }
