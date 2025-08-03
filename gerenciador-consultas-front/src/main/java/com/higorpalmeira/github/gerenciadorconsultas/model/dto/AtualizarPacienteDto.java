1838e684adb93373b61b0d89a049452e66bcdc3b
Higor Palmeira <higorpresente05@gmail.com>
2025-08-02 23:04:52 -0300

    Update AtualizarPacienteDto.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarPacienteDto.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarPacienteDto.java
@@ -4,10 +4,30 @@
  */
 package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
 
+import com.higorpalmeira.github.gerenciadorconsultas.model.enums.Genero.TipoGenero;
+import com.higorpalmeira.github.gerenciadorconsultas.model.enums.TipoStatus.TipoStatusConta;
+import java.time.LocalDate;
+
 /**
  *
  * @author higor
  */
 public class AtualizarPacienteDto {
+    
+    private String nome;
+    
+    private String sobrenome;
+    
+    private String cpf;
+    
+    private LocalDate dataNascimento;
+    
+    private TipoGenero genero;
+    
+    private TipoStatusConta status;
+    
+    private String email;
+    
+    private String telefone;
     
 }
