4cf0a83f5f480fccc799c3603be59bb8c73aa347
Higor Palmeira <higorpresente05@gmail.com>
2025-08-02 23:07:22 -0300

    Update AtualizarEnderecoDto.java

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarEnderecoDto.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarEnderecoDto.java
@@ -10,4 +10,76 @@ package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
  */
 public class AtualizarEnderecoDto {
     
+    private String cep;
+    
+    private String rua;
+    
+    private String complemento;
+    
+    private String bairro;
+    
+    private String localidade;
+    
+    private String uf;
+
+    public AtualizarEnderecoDto() {
+    }
+
+    public AtualizarEnderecoDto(String cep, String rua, String complemento, String bairro, String localidade, String uf) {
+        this.cep = cep;
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
 }
