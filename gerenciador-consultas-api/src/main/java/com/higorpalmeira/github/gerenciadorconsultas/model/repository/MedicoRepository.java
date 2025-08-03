2b7c2cfe0a22decb197671b5a991ae577960fb75
Higor Palmeira <higorpresente05@gmail.com>
2025-08-03 11:27:09 -0300

    Update MedicoRepository.java
    Documentando métodos.

--- gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/MedicoRepository.java
+++ gerenciador-consultas-api/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/repository/MedicoRepository.java
@@ -31,13 +31,19 @@ public interface MedicoRepository extends JpaRepository<Medico, UUID> {
 	 * */
 	boolean existsByEmail(String email);
 	
+	/*
+	 * Verifica se existe um médico com o Telefone fornecido.
+	 * 
+	 * @param telefone O Telefone a ser verificado.
+	 * @return true se um médico existir com este telefone, caso contrário false.
+	 * */
 	boolean existsByTelefone(String telefone);
 	
 	/*
 	 * Busca por um médico com o CRM fornecido.
 	 * 
 	 * @param crm O CRM a ser procurado.
-	 * @return Optional com Doctor presente se o médico for encontrado, caso contrário um Optional vazio.
+	 * @return Optional com Médico presente se o médico for encontrado, caso contrário um Optional vazio.
 	 * */
 	Optional<Medico> findByCrm(String crm);
 	
@@ -45,18 +51,48 @@ public interface MedicoRepository extends JpaRepository<Medico, UUID> {
 	 * Busca por um médico com o E-mail fornecido.
 	 * 
 	 * @param email O E-mail a ser procurado.
-	 * @return Optional com Doctor presente se o médico for encontrado, caso contrário um Optional vazio.
+	 * @return Optional com Médico presente se o médico for encontrado, caso contrário um Optional vazio.
 	 * */
 	Optional<Medico> findByEmail(String email);
 	
+	/*
+	 * Busca por um médico com Telefone fornecido.
+	 * 
+	 * @param telefone O Telefone a ser procurado.
+	 * @return Optional com Médico presente se o médico for encontrado, caso contrário um Optional vazio.
+	 * */
 	Optional<Medico> findByTelefone(String telefone);
 	
+	/*
+	 * Busca por todos os médicos com o Nome exato do fornecido.
+	 * 
+	 * @param nome O Nome exato a ser procurado.
+	 * @return Lista com todos os médicos com o nome fornecido, ou uma lista vazia.
+	 * */
 	List<Medico> findByNome(String nome);
 	
+	/*
+	 * Busca por todos os médicos que contenham o Nome fornecido, ignorando case (maiúsculas ou minúsculas).
+	 * 
+	 * @param nome O Nome a ser procurado.
+	 * @return Lista com todos os médicos com o nome fornecido, ou uma lista vazia.
+	 * */
 	List<Medico> findByNomeContainingIgnoreCase(String nome);
 	
+	/*
+	 * Busca por todos os médicos com o Sobrenome exato do fornecido.
+	 * 
+	 * @param sobrenome O Sobrenome exato a ser procurado.
+	 * @return Lista com todos os médicos com o nome fornecido, ou uma lista vazia.
+	 * */
 	List<Medico> findBySobrenome(String sobrenome);
 	
+	/*
+	 * Busca por todos os médicos que contenham o Sobrenome fornecido, ignorando case (maiúsculas ou minúsculas).
+	 * 
+	 * @param sobrenome O Sobrenome a ser procurado.
+	 * @return Lista com todos os médicos com o sobrenome fornecido, ou uma lista vazia.
+	 * */
 	List<Medico> findBySobrenomeContainingIgnoreCase(String sobrenome);
 	
 	/*
