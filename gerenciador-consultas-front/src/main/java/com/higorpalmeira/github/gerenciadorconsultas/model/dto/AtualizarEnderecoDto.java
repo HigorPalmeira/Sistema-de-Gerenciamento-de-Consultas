4a870932d7fc6860b961a4d7816c1546c5c28f2b
Higor Palmeira <higorpresente05@gmail.com>
2025-08-02 23:05:19 -0300

    Create AtualizarEnderecoDto.java

---
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/model/dto/AtualizarEnderecoDto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.higorpalmeira.github.gerenciadorconsultas.model.dto;
+
+/**
+ *
+ * @author higor
+ */
+public class AtualizarEnderecoDto {
+    
+}
