ca0cc5ba83204fc50a026b197be1de985f05dbcc
Higor Palmeira <higorpresente05@gmail.com>
2025-08-10 16:43:53 -0300

    Update ConsultaClient.java
    Criando método de criar consulta

--- gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/client/ConsultaClient.java
+++ gerenciador-consultas-front/src/main/java/com/higorpalmeira/github/gerenciadorconsultas/client/ConsultaClient.java
@@ -6,13 +6,20 @@ package com.higorpalmeira.github.gerenciadorconsultas.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.higorpalmeira.github.gerenciadorconsultas.model.dto.criar.CriarConsultaDto;
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  *
  * @author higor
  */
 public class ConsultaClient {
+    
+    private final String URL_API = "http://localhost:8080/v1/consulta";
     
     private final HttpClient client;
     
@@ -22,6 +29,20 @@ public class ConsultaClient {
         this.client = HttpClient.newHttpClient();
         this.mapper = new ObjectMapper();
         this.mapper.registerModule(new JavaTimeModule());
+    }
+    
+    public HttpResponse<String> criarConsulta(CriarConsultaDto criarConsultaDto) throws IOException, InterruptedException {
+        
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL_API))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(mapper.writeValueAsString(criarConsultaDto)))
+                .build();
+        
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        return response;
+        
     }
     
 }
